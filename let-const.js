ee6d76c3aed7c35d21d2bd02a011cedf3ccad887
Tasnim813 <tasnim3j@gmail.com>
2026-04-02 11:05:02 +0600

    let-conts

--- let-const.js
+++ let-const.js
@@ -1,3 +1,25 @@
 // es6
+// console.log(name)
+
+// console.log(device)
+// var device='laptop';
+
  const name ='Bangladesh';
- name='BD';
+const  countryName= 'BD ' + name;
+console.log(countryName)
+
+let price=150;
+price=200;
+console.log(price)
+
+
+const charge=50;
+// charge=45;
+
+const dress=['shirt','pant','belt']
+dress.push('maflar');
+console.log(dress)
+
+const student={name:'Tasnim', marks:50}
+student.marks=80;
+console.log(student)
