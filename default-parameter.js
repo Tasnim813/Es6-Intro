048098d2c5c9269e4f65bb7ecb7fb42c20c7638f
Tasnim813 <tasnim3j@gmail.com>
2026-04-05 10:54:53 +0600

    default-parameter

---
+++ default-parameter.js
@@ -0,0 +1,12 @@
+function sum(num1,num2=0){
+    const total=num1+num2;
+    // console.log(total)
+}
+
+sum(10)
+
+function multiply(num1=1,num2=1){
+    const total=num1*num2;
+    console.log(total)
+}
+multiply(10)
