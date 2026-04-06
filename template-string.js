a856c073e6a787541f85b02622f59f5372667a47
Tasnim813 <tasnim3j@gmail.com>
2026-04-06 21:46:12 +0600

    template-string

---
+++ template-string.js
@@ -0,0 +1,26 @@
+// const poem='am pata jora
+// kjfodsf'
+
+// const poem='am pata jora \n' +
+// 'marbo chabuk \n' +
+// 'chorbe gura'
+
+
+// const poem=`am pata jors jora 
+// marbo chbuk 
+// corbe gura`
+
+// console.log(poem)
+
+function sum(num1,num2){
+    const result=num1+num2;
+    // const output='sum of' +num1 + 'and' + num2 +'is equal to' + result;
+    const output= `sum of ${num1*5} and ${num2} is equal to ${result}`
+    console.log(output)
+}
+
+sum(10,20)
+
+const price=50;
+const discountedPrice= `Discounted price ${price} is after 20% of  ${price*0.8}`
+console.log(discountedPrice)
