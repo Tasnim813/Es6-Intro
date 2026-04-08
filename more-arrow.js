c93e442e8d88d5b7667bdc55c044a499fe0e5950
Tasnim813 <tasnim3j@gmail.com>
2026-04-08 20:48:10 +0600

    more-arrow-function

---
+++ more-arrow.js
@@ -0,0 +1,24 @@
+const getTax=(amount,taxRate)=>amount*taxRate/100;
+const add=(p,q)=>p+q;
+
+// Single parameter
+const getSquare=(x)=>x*x;
+const getHalf=num=>num/2;
+const firstElement= nums=>nums[0];
+const logIt=()=>console.log(78)
+
+
+console.log(getSquare(8))
+console.log(getHalf(10))
+
+console.log(firstElement([26,2,53,265,52]))
+logIt()
+
+// anonymus function
+// document.getElementById('').addEventListener('click',()=>{
+
+// })
+
+document.getElementById('btn').addEventListener('click', event=>{
+    
+})
