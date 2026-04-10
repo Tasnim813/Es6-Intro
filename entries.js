ada237daaab65cc22cb02fc4ca18ecf3e2bba354
Tasnim813 <tasnim3j@gmail.com>
2026-04-10 19:28:26 +0600

    key values entries

---
+++ entries.js
@@ -0,0 +1,15 @@
+const employee={
+    name:'Tasnim',
+    designation:'QA',
+    salary:50000,
+    experience:1
+
+}
+
+const keys=Object.keys(employee)
+console.log(keys)
+const values=Object.values(employee)
+console.log(values)
+
+const entries=Object.entries(employee)
+console.log(entries)
