18bc100b5a91d57bcf5356991b142f71cc422b59
Tasnim813 <tasnim3j@gmail.com>
2026-04-10 19:12:20 +0600

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,25 @@
+const {price,quantity}={name:'Shirt',price:500,quantity:7}  
+// console.log(price,quantity)
+// const discount=product.price*20/100;
+// const yourPay=product.price-discount;
+// const vatAmount=product.price*5/100;
+// const totalAmount=yourPay+vatAmount;
+
+
+// const price=product.price;
+
+// const discount=price*20/100;
+// const yourPay=price-discount;
+// const vatAmount=price*5/100;
+// const totalAmount=yourPay+vatAmount;
+
+
+const device={name:'phone', brand:'samsung', price:1700}
+const {name,brand}=device 
+
+// ---------------------
+const numbers=[20,45]
+const [first,second]=numbers
+
+const [math,physics]=[40,50]
+console.log(physics)
