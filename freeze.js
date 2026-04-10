6e7e15d1993c6132d3a6b7c4db2fcdfd88182190
Tasnim813 <tasnim3j@gmail.com>
2026-04-10 19:34:06 +0600

    freeze

---
+++ freeze.js
@@ -0,0 +1,13 @@
+const employee={
+    name:'Tasnim',
+    designation:'QA',
+    salary:50000,
+    experience:1,
+    age:25
+
+}
+Object.freeze(employee)
+delete employee.experience
+employee.salary=employee.salary+10000;
+employee.location='sylhet'
+console.log(employee)
